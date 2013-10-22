cf4113e3dcbae20b89f36f7500c97f8cf124a19e
Jérôme Combes <jerome@planningbiblio.fr>
2013-10-22 17:04:56 +0200

    Possibilité de trier le tableau des plannings de présences (tableau des administrateurs) Affichage du tableau avec le plugin dataTables

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ Copyright (C) 2013 - Jérôme Combes
 
 Fichier : plugins/planningHebdo/index.php
 Création : 23 juillet 2013
-Dernière modification : 8 ocotbre 2013
+Dernière modification : 22 ocotbre 2013
 Auteur : Jérôme Combes, jerome@planningbilbio.fr
 
 Description :
@@ -18,15 +18,29 @@ Page accessible à partir du menu administration/planning de présence
 include "class.planningHebdo.php";
 
 // Initialisation des variables
-$agent=isset($_GET['agent'])?$_GET['agent']:null;
 $tri=isset($_GET['tri'])?$_GET['tri']:"`debut`,`fin`,`nom`,`prenom`";
-$debut=isset($_GET['debut'])?$_GET['debut']:null;
-$fin=isset($_GET['fin'])?$_GET['fin']:null;
+$debut=isset($_GET['debut'])?$_GET['debut']:(array_key_exists("planningHebdoDebut",$_SESSION['oups'])?$_SESSION['oups']['planningHebdoDebut']:null);
+$fin=isset($_GET['fin'])?$_GET['fin']:(array_key_exists("planningHebdoFin",$_SESSION['oups'])?$_SESSION['oups']['planningHebdoFin']:null);
+$agent=isset($_GET['agent'])?$_GET['agent']:(array_key_exists("planningHebdoAgent",$_SESSION['oups'])?$_SESSION['oups']['planningHebdoAgent']:null);
+if(isset($_GET['reset'])){
+  $debut=null;
+  $fin=null;
+  $agent=null;
+}
+$_SESSION['oups']['planningHebdoDebut']=$debut;
+$_SESSION['oups']['planningHebdoFin']=$fin;
+$_SESSION['oups']['planningHebdoAgent']=$agent;
 
+// Les dates arrivent en format J/MM/AAAA, 
+// DatePicker les veut en format MM/JJ/AAAA pour les convertir ensuite en JJ/MM/AAAA
+$debutDatePicker=preg_replace("/([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{2,4})/","$2/$1/$3",$debut);
+$finDatePicker=preg_replace("/([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{2,4})/","$2/$1/$3",$fin);
+
+// Recherche des plannings
 $p=new planningHebdo();
 $p->agent=$agent;
-$p->debut=$debut;
-$p->fin=$fin;
+$p->debut=dateFr($debut);
+$p->fin=dateFr($fin);
 $p->tri=$tri;
 $p->fetch();
 
@@ -49,20 +63,23 @@ echo <<<EOD
 <h3>Plannings de présence</h3>
 <form name='form' method='get' action='index.php'>
 <input type='hidden' name='page' value='plugins/planningHebdo/index.php' />
-Début : <input type='text' name='debut' value='$debut' />&nbsp;<img src='img/calendrier.gif' onclick='calendrier("debut");' alt='calendrier' />
-&nbsp;&nbsp;Fin : <input type='text' name='fin' value='$fin' />&nbsp;<img src='img/calendrier.gif' onclick='calendrier("fin");' alt='calendrier' />
+Début : <input type='text' name='debut' class='datepicker' value='$debutDatePicker' />
+&nbsp;&nbsp;Fin : <input type='text' name='fin' class='datepicker' value='$finDatePicker' />
 &nbsp;&nbsp;Agent : <input type='text' name='agent' value='$agent' />
 &nbsp;&nbsp;<input type='submit' value='OK' />
-&nbsp;&nbsp;<input type='button' value='Effacer' onclick='location.href="index.php?page=plugins/planningHebdo/index.php"' />
-<a href='index.php?page=plugins/planningHebdo/configuration.php' style='margin-left:100px;'>Configuration</a>
+&nbsp;&nbsp;<input type='button' value='Effacer' onclick='location.href="index.php?page=plugins/planningHebdo/index.php&amp;reset="' />
+<a href='index.php?page=plugins/planningHebdo/configuration.php' style='position:absolute;right:10px;'>Configuration</a>
 </form>
 
-<table class='tableauStandard'>
-<tr class='th' style='vertical-align:top;text-align:center;'><td>&nbsp;</td><td>Agent</td><td>Service</td><td>Début</td><td>Fin</td><td>Saisie</td><td>Validation</td><td>Actuel</td><td>Commentaires</td></tr>
+<br/>
+
+<table id='tablePlanningHebdo'>
+<thead>
+  <tr><th>&nbsp;</th><th>Agent</th><th>Service</th><th>Début</th><th>Fin</th><th>Saisie</th><th>Validation</th><th>Actuel</th><th>Commentaires</th></tr>
+  </thead>
+<tbody>
 EOD;
-$class="tr1";
 foreach($p->elements as $elem){
-  $class=$class=="tr1"?"tr2":"tr1";
   $actuel=$elem['actuel']?"Oui":null;
   $validation="<b>N'est pas validé</b>";
   if($elem['valide']){
@@ -72,7 +89,7 @@ foreach($p->elements as $elem){
   $commentaires=$elem['remplace']?"Remplace le planning <br/>du $planningRemplace":null;
   $arrow=$elem['remplace']?"<font style='font-size:20pt;'>&rdsh;</font>":null;
 
-  echo "<tr style='text-align:center;' class='$class'>";
+  echo "<tr>";
   echo "<td style='white-space:nowrap;'>$arrow <a href='index.php?page=plugins/planningHebdo/modif.php&amp;id={$elem['id']}&amp;retour=index.php'/>";
     echo "<img src='img/modif.png' alt='Voir' border='0'/></a>";
     echo "<a href='javascript:plHebdoSupprime({$elem['id']});' style='margin-left:6px;'/>";
@@ -87,5 +104,23 @@ foreach($p->elements as $elem){
   echo "<td>$commentaires</td>";
   echo "</tr>\n";
 }
-echo "</table>\n";
+echo "</tbody></table>\n";
 ?>
+
+<script type='text/JavaScript'>
+$(document).ready(function() {
+  $(".datepicker").datepicker();
+  $(".datepicker").datepicker("option", "dateFormat", "dd/mm/yy");
+
+  $("#tablePlanningHebdo").dataTable({
+    "bJQueryUI": true,
+    "sPaginationType": "full_numbers",
+    "bStateSave": false,
+    "aaSorting" : [[3,"asc"],[4,"asc"],[1,"asc"]],
+    "aoColumns" : [{"bSortable":false},{"bSortable":true},{"bSortable":true},{"bSortable":true},{"bSortable":true},{"bSortable":true},{"bSortable":true},{"bSortable":true},{"bSortable":true},],
+    "aLengthMenu" : [[25,50,75,100,-1],[25,50,75,100,"Tous"]],
+    "iDisplayLength" : 25,
+    "oLanguage" : {"sUrl" : "js/dataTables/french.txt"}
+  });
+});
+</script>
